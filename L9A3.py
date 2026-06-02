d7923f389d2ad8325dffd7c42bcf93e798e3415b
Pristql <luigimansion111@gmail.com>
2026-06-02 18:00:07 +0530

    Add files via upload

---
+++ L9A3.py
@@ -0,0 +1,23 @@
+print("Choose your ride")
+print("1-Bike")
+print("2-Car")
+choice=int(input("Enter your choice here: "))
+if choice==1:
+    print("What type of bike?")
+    print("1-Scooty")
+    print("2-Scooter")
+choice2=int(input("Enter your choice2 here: "))
+if choice2==1:
+    print("You have selected scooty")
+elif print("You have selected scooter")
+if choice==2:
+    print("What type of car?")
+    print("1-Toyota")
+    print("2-Suzuki")
+choice3=int(input("Enter your choice3 here: "))
+if choice3==1:
+    print("Yo have a toyota")
+elif choice3==2:
+    print("You have a Suzuki!")
+else:
+    print("Wrong answer")
